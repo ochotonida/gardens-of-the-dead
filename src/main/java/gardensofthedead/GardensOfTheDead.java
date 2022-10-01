a9159a4e2d1a41a5f8cb4ae80469c4301d6dd045
Wouter Pauwels <37985539+ochotonida@users.noreply.github.com>
2022-10-01 15:57:34 +0200

    Initial commit

---
+++ src/main/java/gardensofthedead/GardensOfTheDead.java
@@ -0,0 +1,10 @@
+package gardensofthedead;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(GardensOfTheDead.MODID)
+public class GardensOfTheDead {
+
+    public static final String MODID = "gardens_of_the_dead";
+
+}
